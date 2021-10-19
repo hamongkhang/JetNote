6a26b36a2dbcf1a6285a320b61a6cd7d0d4b6d60
NguyenQuan <hamongkhang@gmail.com>
2021-10-19 10:24:54 +0700

    create jetNote by Kotlin

---
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "Home"
+    }
+    val text: LiveData<String> = _text
+}
